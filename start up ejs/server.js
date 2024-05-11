ccc11a559d5f64e7ceb67010c0d8d5c669d9222b
dansan-dsn <ddryn970@gmail.com>
2024-05-11 13:09:28 +0300

    loops and if statements

--- start up ejs/server.js
+++ start up ejs/server.js
@@ -1,12 +1,30 @@
+
 var express = require('express')
 var path = require('path')
 var bodyParser = require('body-parser')
 var cors = require('cors')
+var PORT = process.env.PORT || 8000
 
 var app = express()
 
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
 app.use(bodyParser())
 app.use(cors())
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        people: [
+            {name : 'dave'},
+            {name : 'jerry'},
+            {name : 'dansan'}
+        ]
+    })
+})
+
+app.listen(PORT, ()=> {
+    console.log(`Server listening on port ${PORT}...`)
+})
